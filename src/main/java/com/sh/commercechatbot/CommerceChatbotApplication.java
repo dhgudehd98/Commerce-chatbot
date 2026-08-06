080cc8add78667ceabd45078285d061e4eb4f56a
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2026-08-06 18:17:49 +0900

    first commit

---
+++ src/main/java/com/sh/commercechatbot/CommerceChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.sh.commercechatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceChatbotApplication.class, args);
+	}
+
+}
